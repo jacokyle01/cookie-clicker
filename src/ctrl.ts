b8470302e74be230160eca34463f687d4702bd43
Kyle Jacobson <jacokyle01@gmail.com>
2023-11-21 19:29:16 -0600

    add model for resources

--- src/ctrl.ts
+++ src/ctrl.ts
@@ -1,14 +1,23 @@
-import { Redraw } from "./interfaces";
+import { Redraw, resourceNames, Inventory } from "./interfaces";
 
 export default class CookieCtrl {
     cookieCount = 0;
-    
-    constructor(readonly redraw: Redraw) {
+    resources: {[key: string]: Inventory} = {};
 
+    constructor(readonly redraw: Redraw) {
+        //TODO: move this to initialization / config class?
+        resourceNames.forEach((resourceName : string, index: number) => {
+            let inventory = {
+                count: 0,
+                price: (index * index) * 15 + 10
+            }
+            this.resources[resourceName] = inventory;
+        })
     }
 
     clickCookie = (): void => {
         this.cookieCount++;
+        console.log(this.resources);
         this.redraw();
     }
 
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,1 +1,8 @@
 export type Redraw = () => void;
+
+//TODO: refactor inventory: add Resource type? 
+export const resourceNames = ["Cursor", "Baker", "Factory"]
+export type Inventory = {
+    count: number;
+    price: number;
+} 
